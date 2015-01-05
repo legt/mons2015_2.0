0aca3be89da8009d0844a867fb8eb85232c84630
duanra14001 <deboeck.arnaud@gmail.com>
2015-01-05 08:55:03 +0100

    Changement apres vac Arnaud

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ $(function closeMenu() {
 
 var contenuSelect = 0;
 
+
 $(function clickApropos() {
 	$("#button_Apropos").click(function(e) {
 		e.preventDefault;
@@ -38,9 +39,70 @@ $(function clickApropos() {
 				$('#Pinteret').hide();
 				contenuSelect = 1;
 			 };
-		
-		
-	})
+    });
+});
+
+
+// Slider + Bulle
+$(function carousel() { 
+		var current       = 0,
+	        buttonActive    = true,
+	        divSlider     = $('.slider'),
+	        sliderImg   = $('.slider_img'),
+	        firstImg    = sliderImg.first(),
+            totalImgs	  = sliderImg.length;
+        
+	   firstImg.css('display','block');
+	   
+       sliderBullets = divSlider.append('<div class="slider-bullets"></div>');
+	
+	   if(totalImgs) {
+		  for(i = 0; i < totalImgs; i++){
+			  $('<span></span>').appendTo('.slider-bullets');
+		  }
+	   }
+
+	   var bullet = $('.slider-bullets span');
+       
+       bullet.first().addClass('is-active');
+	
+    
+	   $('[data-src]').each(function(){
+		  $(this).css('background-image', 'url(' + $(this).data('src') + ')');
+	   });
+        
+    
+       $('.slider-bullets span').click(function(){
+           
+                   var $this  = $(this),
+                       bullet_index = $this.index();
+
+		  
+				$("span.is-active").removeClass("is-active");
+			    $(this).addClass("is-active");
+			    sliderImg.hide().css('z-index', 2000000);
+				
+                sliderImg.eq(bullet_index).css('z-index', 3000000).fadeIn().addClass('img_active');	
+                
+                setTimeout(function(){
+                    buttonActive = true;
+                }, 600);
+       });
+    // slide automatique 
+        
+//        setInterval(function() {
+    
+//                $('.slider_container_img :first')
+//                        .fadeOut(2000)
+//                        .next()
+//                        .fadeIn(2000)
+//                        .end()
+//                        .appendTo('.slider_container_img');
+//                
+//            },  5000);
+    
+    
+    
 });
 
 $(function clickPinteret() {
