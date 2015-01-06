1c6aa82eabb8081360879871b2057d25c4a884ce
duanra14001 <deboeck.arnaud@gmail.com>
2015-01-06 10:38:46 +0100

    SLIDER OK

--- js/main.js
+++ js/main.js
@@ -70,7 +70,9 @@ $(function clickPinteret() {
 		
 	});
 });
- // slise 
+ 
+// slise 
+
 var listImg_slider = ["img/15213124199_8907cf9d3a_o.jpg","img/15213201559_b2e7086112_o.jpg","img/15213216999_ff1e0ceefa_o.jpg"];
 
 $(function slider() {
@@ -83,7 +85,6 @@ $(function slider() {
 	};
 	
 	$('.slider_bullets span').first().addClass('is_active');
-	$('.slider_img').addClass('img_active');
 	
 			img_slider = $('.slider_img');
 			bullet = $('.slider_bullets span');
@@ -93,16 +94,17 @@ $(function slider() {
 	// Slide automatique			
 			
 			setInterval(function() {
-				 			img_slider.removeClass('img_active');
-         			
+         			var $next = $("span.is_active").next();
 							img_slider.fadeOut(1000, function(){
-									$("span.is_active").removeClass("is_active").next(j++).addClass("is_active");
-									img_slider.css('background-image', 'url('+listImg_slider[i++]+')');
+									$("span.is_active").removeClass("is_active");
+									img_slider.css('background-image', 'url('+listImg_slider[i+++1]+')');
 									img_slider.fadeIn(1000);
-									console.log(j);
-								if(i==listImg_slider.length){
-									i=0;
-							};
+									
+									if(i==listImg_slider.length){
+										i=0;
+										$('.slider_img').css('background-image', 'url('+listImg_slider[0]+')');
+									};
+								$("span#bullet_"+i).addClass("is_active");
 							});				
       }, 5000);
 		
