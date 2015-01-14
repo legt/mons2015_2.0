b56c23dbb9f9b650e0a52313e71ee2b4540dfeea
duanra14001 <deboeck.arnaud@gmail.com>
2015-01-14 16:19:31 +0100

    FB connect
    Connection ok like and share button ok

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 		menu.init();
         btn_click.init();
         slider.init();
+        face_connection.init();
         
 	});
 
@@ -208,5 +209,67 @@ var slider = {
 		$('.slider_img').css('background-image', 'url(' + listImg_slider[bullet_index] + ')').fadeIn(3000);
     }
 };
+
+var face_connection = {
+        init : function() {
+            
+            function statusChangeCallback(response) {
+                console.log('statusChangeCallback');
+                console.log(response);
+                // The response object is returned with a status field that lets the
+                // app know the current login status of the person.
+                // Full docs on the response object can be found in the documentation
+                // for FB.getLoginStatus().
+                if (response.status === 'connected') {
+                  // Logged into your app and Facebook.
+                  testAPI();
+                } else if (response.status === 'not_authorized') {
+                  // The person is logged into Facebook, but not your app.
+                  document.getElementById('status').innerHTML = 'Please log ' +
+                    'into this app.';
+                } else {
+                  // The person is not logged into Facebook, so we're not sure if
+                  // they are logged into this app or not.
+                  document.getElementById('status').innerHTML = 'Please log ' +
+                    'into Facebook.';
+                }
+              }
+            
+            window.fbAsyncInit = function() {
+                
+                FB.init({
+                    appId      : '321376851405004',
+                    cookie     : true,
+                    xfbml      : true,
+                    version    : 'v2.1'
+                });
+            };
+            
+             function checkLoginState() {
+                FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+            });
+            }
+            
+
+            (function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&appId=321376851405004&version=v2.0";
+                  fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+            
+            function testAPI() {
+                console.log('Welcome!  Fetching your information.... ');
+                FB.api('/me', function(response) {
+                  console.log('Successful login for: ' + response.name);
+                  document.getElementById('status').innerHTML =
+                    'Thanks for logging in, ' + response.name + '!';
+                });
+            }
+        }
+        
+}
 
 })(jQuery);
